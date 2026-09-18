5b261fa6945173902e36c1a4718ac3497277bc6e
Theresia Angel Simbolon <2581015@unai.edu>
2026-09-18 09:27:48 +0700

    Menambahkan data NIK dan Umur

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Data 1: NIK
+        System.out.print("NIK (16 digit) :");
+        long nik = scanner.nextLong();
+
+        //Dta 2: Umur
+        System.out.print("Umur           :");
+        int umur = scanner.nextInt();
+
+        //Data 3:
+    }
 }
